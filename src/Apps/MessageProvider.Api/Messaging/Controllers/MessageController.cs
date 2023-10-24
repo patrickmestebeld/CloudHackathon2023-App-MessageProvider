864b1fdb41b1736f10900b7ec3edde757acaac29
Patrick Mestebeld <ps.mestebeld@belastingdienst.nl>
2023-10-24 20:57:54 +0200

    tweak api

--- src/Apps/MessageProvider.Api/Messaging/Controllers/MessageController.cs
+++ src/Apps/MessageProvider.Api/Messaging/Controllers/MessageController.cs
@@ -2,29 +2,29 @@ using MessageProvider.Api.MessageService.Models;
 using MessageProvider.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MessageProvider.Api.Controllers;
+namespace MessageProvider.Api.Messaging.Controllers;
 
 
 [ApiController]
-[Route("[controller]")]
-public class MessageController : ControllerBase
+[Route("events")]
+public class MessageEventController : ControllerBase
 {
     private readonly IMessageService _messageService;
 
-    public MessageController(IMessageService messageService)
+    public MessageEventController(IMessageService messageService)
     {
         _messageService = messageService;
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetMessage()
+    public async Task<IActionResult> GetMessageEvent()
     {
         var message = await _messageService.ReceiveNextMessage();
         return Ok(message);
     }
 
     [HttpPost]
-    public async Task<IActionResult> PostMessage([FromBody] string message)
+    public async Task<IActionResult> PostMessageEvent([FromBody] string message)
     {
         await _messageService.SendNextMessageAsync(new Message(message));
         return Ok();
