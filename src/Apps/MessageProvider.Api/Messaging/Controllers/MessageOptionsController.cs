0d5e8ea5ec06647f841600a7729760485ffdd90e
Patrick Mestebeld <ps.mestebeld@belastingdienst.nl>
2023-10-25 10:56:31 +0200

    add debug controller

---
+++ src/Apps/MessageProvider.Api/Messaging/Controllers/MessageOptionsController.cs
@@ -0,0 +1,24 @@
+﻿using MessageProvider.Api.MessageService.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace MessageProvider.Api.Messaging.Controllers;
+
+
+[ApiController]
+[Route("options")]
+public class MessageOptionsController : ControllerBase
+{
+    private readonly MessageServiceOptions _options;
+
+    public MessageOptionsController(IOptions<MessageServiceOptions> _options)
+    {
+        this._options = _options.Value;
+    }
+
+    [HttpGet]
+    public ActionResult<MessageServiceOptions> GetOptions()
+    {
+        return Ok(_options);
+    }
+}
